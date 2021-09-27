271444796be66ad328faf07d27d85a09f6e868a5
Ravish Ranjan <ravish@mobilewalla.com>
2021-09-27 09:20:04 +0530

    unit test func duplicate_detection and invalid_entries_detection(quality_checker)

--- src/test/com/mw/ds/data_analyzer/test_quality_checker.py
+++ src/test/com/mw/ds/data_analyzer/test_quality_checker.py
@@ -9,10 +9,6 @@ sample_output_path = "./data/tmp/output/"
 
 @pytest.mark.usefixtures("spark_session")
 
-# def rows_wMissingFeats(idf, list_of_cols='all', drop_cols=[], treatment=False, treatment_threshold=0.8, print_impact=False):
-# def duplicate_detection(idf, list_of_cols='all', drop_cols=[], treatment=False, print_impact=False):
-# def invalidEntries_detection(idf, list_of_cols='all', drop_cols=[], treatment=False, 
-#                              output_mode='replace', print_impact=False):
 # def IDness_detection(idf, list_of_cols='all', drop_cols=[], treatment=False, treatment_threshold=1.0, print_impact=False):
 # def biasedness_detection(idf, list_of_cols='all', drop_cols=[], treatment=False, treatment_threshold=1.0, print_impact=False):
 # def outlier_detection(idf, list_of_cols='all', drop_cols=[], detection_side='upper', 
@@ -44,7 +40,55 @@ def test_rows_wMissingFeats(spark_session):
     assert result_df[1].where(F.col("null_feats_count") == 0).toPandas().to_dict('list')['flagged'][0] == 0
     assert result_df[1].where(F.col("null_feats_count") == 2).toPandas().to_dict('list')['row_count'][0] == 1   
     assert result_df[1].where(F.col("null_feats_count") == 2).toPandas().to_dict('list')['row_pct'][0] == 0.25
-    assert result_df[1].where(F.col("null_feats_count") == 2).toPandas().to_dict('list')['flagged'][0] == 1   
+    assert result_df[1].where(F.col("null_feats_count") == 2).toPandas().to_dict('list')['flagged'][0] == 1 
+    
+def test_duplicate_detection(spark_session):
+    test_df1 = spark_session.createDataFrame(
+        [
+            ('27520a', 51, 9000, 'HS-grad'),
+            ('10a', 42, 7000,'Postgrad'),
+            ('10a', 42, 7000,'Postgrad'),
+            ('11a', 35, None, None),
+            ('1100b', 23, 6000, 'HS-grad')
+        ],
+        ['ifa', 'age', 'income','education']
+    )
+    assert test_df1.where(F.col("ifa") == "27520a").count() == 1
+    assert test_df1.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['age'][0] == 51
+    assert test_df1.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['income'][0] == 9000  
+    assert test_df1.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['education'][0] == 'HS-grad' 
+
+    result_df1 = duplicate_detection(test_df1,treatment = True)
+
+    assert result_df1[0].count() == 4
+    assert result_df1[1].where(F.col("metric") == "rows_count").toPandas().to_dict('list')['value'][0] == 5   
+    assert result_df1[1].where(F.col("metric") == "unique_rows_count").toPandas().to_dict('list')['value'][0] == 4
+    
+def test_invalidEntries_detection(spark_session):
+    test_df2 = spark_session.createDataFrame(
+        [
+            ('27520a', 51, 9000, 'HS-grad'),
+            ('10a', 42, 7000,'Postgrad'),
+            ('10a', 9999, 7000,'Postgrad'),
+            ('11a', 35, None, ':'),
+            ('1100b', 23, 6000, 'HS-grad')
+        ],
+        ['ifa', 'age', 'income','education']
+    )
+    assert test_df2.where(F.col("ifa") == "27520a").count() == 1
+    assert test_df2.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['age'][0] == 51
+    assert test_df2.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['income'][0] == 9000  
+    assert test_df2.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['education'][0] == 'HS-grad' 
+
+    result_df2 = invalidEntries_detection(test_df2,treatment = True)
+
+    assert result_df2[0].count() == 5
+    assert result_df2[1].where(F.col("feature") == "age").toPandas().to_dict('list')['invalid_count'][0] == 1 
+    assert result_df2[1].where(F.col("feature") == "age").toPandas().to_dict('list')['invalid_pct'][0] == 0.2
+    assert result_df2[1].where(F.col("feature") == "education").toPandas().to_dict('list')['invalid_count'][0] == 1 
+    assert result_df2[1].where(F.col("feature") == "education").toPandas().to_dict('list')['invalid_pct'][0] == 0.2
+    
+    
 
 
 
