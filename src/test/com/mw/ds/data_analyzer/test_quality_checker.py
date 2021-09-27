812d703e0b8fa628c374b6dea544e8e3afa7120c
Ravish Ranjan <ravish@mobilewalla.com>
2021-09-27 16:32:20 +0530

    unit test func Idness_detection and biasedness_detection(Quality_checker)

--- src/test/com/mw/ds/data_analyzer/test_quality_checker.py
+++ src/test/com/mw/ds/data_analyzer/test_quality_checker.py
@@ -9,8 +9,6 @@ sample_output_path = "./data/tmp/output/"
 
 @pytest.mark.usefixtures("spark_session")
 
-# def IDness_detection(idf, list_of_cols='all', drop_cols=[], treatment=False, treatment_threshold=1.0, print_impact=False):
-# def biasedness_detection(idf, list_of_cols='all', drop_cols=[], treatment=False, treatment_threshold=1.0, print_impact=False):
 # def outlier_detection(idf, list_of_cols='all', drop_cols=[], detection_side='upper', 
 # def imputation_MMM(idf, list_of_cols="missing", drop_cols=[], method_type="median",
 # def null_detection(idf, list_of_cols='all', drop_cols=[], treatment=False, treatment_method='row_removal', 
@@ -87,6 +85,52 @@ def test_invalidEntries_detection(spark_session):
     assert result_df2[1].where(F.col("feature") == "age").toPandas().to_dict('list')['invalid_pct'][0] == 0.2
     assert result_df2[1].where(F.col("feature") == "education").toPandas().to_dict('list')['invalid_count'][0] == 1 
     assert result_df2[1].where(F.col("feature") == "education").toPandas().to_dict('list')['invalid_pct'][0] == 0.2
+    
+def test_IDness_detection(spark_session):
+    test_df3 = spark_session.createDataFrame(
+        [
+            ('27520a', 51, 9000, 'HS-grad'),
+            ('10a', 42, 7000,'Postgrad'),
+            ('11a', 35, None, "graduate"),
+            ('1100b', 23, 6000, 'matric')
+        ],
+        ['ifa', 'age', 'income','education']
+    )
+    assert test_df3.where(F.col("ifa") == "27520a").count() == 1
+    assert test_df3.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['age'][0] == 51
+    assert test_df3.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['income'][0] == 9000  
+    assert test_df3.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['education'][0] == 'HS-grad' 
+
+    result_df3 = IDness_detection(test_df3,drop_cols=['ifa'],treatment=True,treatment_threshold=1.0)
+
+    assert len(result_df3[0].columns) == 3
+    assert result_df3[1].where(F.col("feature") == "education").toPandas().to_dict('list')['unique_values'][0] == 4 
+    assert result_df3[1].where(F.col("feature") == "education").toPandas().to_dict('list')['IDness'][0] == 1.0
+    assert result_df3[1].where(F.col("feature") == "education").toPandas().to_dict('list')['flagged'][0] == 1
+    
+def test_biasedness_detection(spark_session):
+    test_df4 = spark_session.createDataFrame(
+        [
+            ('27520a', 51, 9000, 'HS-grad'),
+            ('10a', 42, 7000,'HS-grad'),
+            ('11a', 35, None, "HS-grad"),
+            ('11d', 45, 9500, "HS-grad"),
+            ('1100b', 23, 6000, 'matric')
+        ],
+        ['ifa', 'age', 'income','education']
+    )
+    assert test_df4.where(F.col("ifa") == "27520a").count() == 1
+    assert test_df4.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['age'][0] == 51
+    assert test_df4.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['income'][0] == 9000  
+    assert test_df4.where(F.col("ifa") == "27520a").toPandas().to_dict('list')['education'][0] == 'HS-grad' 
+
+    result_df4 = biasedness_detection(test_df4,treatment=True,treatment_threshold=0.8)
+
+    assert len(result_df4[0].columns) == 3
+    assert result_df4[1].where(F.col("feature") == "education").toPandas().to_dict('list')['mode'][0] == 'HS-grad' 
+    assert result_df4[1].where(F.col("feature") == "education").toPandas().to_dict('list')['mode_pct'][0] == 0.8
+    assert result_df4[1].where(F.col("feature") == "education").toPandas().to_dict('list')['flagged'][0] == 1
+
     
     
 
