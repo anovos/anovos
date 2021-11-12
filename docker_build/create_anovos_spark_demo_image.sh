bc87fcf721790529cadfb7dcc480627c0ea18c47
Vishnu Gowthem Thangaraj <vishnug@mobilewalla.com>
2021-11-12 14:53:37 +0800

    spark demo shell script update

--- docker_build/create_anovos_spark_demo_image.sh
+++ docker_build/create_anovos_spark_demo_image.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo && echo ... Building Docker image ... && echo
+
+echo && echo ... Cleaning Up Current ANOVOS Docker Images ... && echo
+docker stop anovos_spark_demo && docker rm anovos_spark_demo && docker rmi anovos-spark-demo
+
+echo && echo ... Building ANOVOS Docker image ... && echo
 docker build -f Dockerfile_spark_demo -t anovos-spark-demo . && echo
 
-echo ... Docker image successfully built ! ... && echo 
+echo ... ANOVOS Docker image successfully built ! ... && echo 
 docker image ls && echo 
 
 echo ... Running ANOVOS on Docker ... && echo 
