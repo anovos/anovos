056b8c3a2c608070ccd00c97c5292bf2ded7adbf
Kilian Kluge <dev@kluge.ai>
2022-01-03 11:40:04 +0100

    Adapt shared SparkSession config to Spark 3

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -1,30 +1,16 @@
 import pathlib
 import sys
 
-
-
 import findspark
 import pytest
-from packaging import version
 
 findspark.init()
-
-import pyspark
-
-if version.parse(pyspark.__version__) < version.parse("3.0.0"):
-    SPARK_JARS_PACKAGES = ["io.github.histogrammar:histogrammar_2.11:1.0.20",
-                           "io.github.histogrammar:histogrammar-sparksql_2.11:1.0.20",
-                           "org.apache.spark:spark-avro_2.11:2.4.0"]
-else:
-    SPARK_JARS_PACKAGES = ["io.github.histogrammar:histogrammar_2.12:1.0.20",
-                           "io.github.histogrammar:histogrammar-sparksql_2.12:1.0.20",
-                           "org.apache.spark:spark-avro_2.12:3.1.2"]
-
 from pyspark.sql import SparkSession
 
 SRC_DIR = pathlib.Path(__file__).parent.parent / 'main'
-
 sys.path.insert(0, str(SRC_DIR.absolute()))
+
+from anovos.shared.spark import SPARK_JARS_PACKAGES
 
 
 @pytest.fixture(scope="session")
