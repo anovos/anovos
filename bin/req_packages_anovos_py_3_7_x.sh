0959b439c44f08fb637397189a8739b6ef90fded
Kilian Kluge <dev@kluge.ai>
2022-01-22 17:01:31 +0100

    First step: Install from requirements.txt

--- bin/req_packages_anovos_py_3_7_x.sh
+++ bin/req_packages_anovos_py_3_7_x.sh
@@ -1,19 +1,5 @@
 sudo pip-3.7 install --upgrade cython
 sudo pip-3.7 install --upgrade pip --user
 sudo pip-3.7 install --upgrade setuptools --user
-sudo pip-3.7 install numpy==1.18.1
-sudo pip-3.7 install pandas==1.2.5
-sudo pip-3.7 install plotly
-sudo pip-3.7 install scipy
-sudo pip-3.7 install --upgrade --no-deps --force-reinstall scikit-learn
-sudo pip-3.7 install boto3
-sudo pip-3.7 install pyarrow #latest version throw module error for Cython
-sudo pip-3.7 install pybind11
-sudo pip-3.7 install popmon
-sudo pip-3.7 install varclushi
-sudo pip-3.7 install soupsieve
-sudo pip-3.7 install datapane==0.12.0
-sudo pip-3.7 install plotly==5.2.1
-sudo pip-3.7 install fsspec
-sudo pip-3.7 install s3fs
+sudo pip-3.7 install -r ../requirements.txt
 sudo pip-3.7 install --no-deps --force-reinstall matplotlib==3.4.3
