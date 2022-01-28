89cb8f4c869fb8cbcc26529cd30234caf2fa90d6
Kilian Kluge <dev@kluge.ai>
2022-01-28 14:22:22 +0100

    Require Python >= 3.7

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup, find_packages
 from os import path
+
+from setuptools import setup
 
 DIR = path.dirname(path.abspath(__file__))
 INSTALL_PACKAGES = open(path.join(DIR, 'requirements.txt')).read().splitlines()
@@ -9,8 +10,9 @@ with open(path.join(DIR, 'README.md')) as f:
 
 setup(
     name='anovos',
-    package_dir={'anovos':'src/main/anovos'},
-    packages=['anovos','anovos.shared','anovos.data_transformer','anovos.data_ingest','anovos.data_analyzer','anovos.data_drift','anovos.data_report'],
+    package_dir={'anovos': 'src/main/anovos'},
+    packages=['anovos', 'anovos.shared', 'anovos.data_transformer', 'anovos.data_ingest', 'anovos.data_analyzer',
+              'anovos.data_drift', 'anovos.data_report'],
     description='An Open Source tool for Feature Engineering in Machine Learning',
     long_description=README,
     long_description_content_type='text/markdown',
@@ -19,10 +21,10 @@ setup(
     url='https://github.com/anovos/anovos.git',
     author='Team Anovos',
     author_email='info@anovos.ai',
-    keywords=['machine learning','open source','feature engineering','analytics'],
+    keywords=['machine learning', 'open source', 'feature engineering', 'analytics'],
     tests_require=[
         'pytest',
         'coverage'
     ],
-    python_requires='>=3'
+    python_requires='>=3.7'
 )
