cd6458ab9d0d7c05b152f510f4cfda46161aa53d
Kilian Kluge <dev@kluge.ai>
2022-02-03 18:38:07 +0100

    Build examples image based on official Docker images

--- examples/create_anovos_examples_docker_image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Build the examples image
-sudo docker build . -t anovos-examples
-
-# Spin up a container and check the versions
-sudo docker run -d --name anovos_examples anovos-examples:latest
-sudo docker exec -it anovos_examples /bin/bash -c "python --version && spark-submit --version && java -version"
-sudo docker stop anovos_examples
