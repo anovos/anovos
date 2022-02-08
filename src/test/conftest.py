e4d37b32c06e26defeb1de89918f8fbd17eefc9e
ziedbouf <zied.boufaden@gmail.com>
2022-02-08 16:57:38 +0100

    fix import path under test/conftest.py

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -1,11 +1,25 @@
+import pathlib
+import sys
+
 import pytest
 from pyspark.sql import SparkSession
+
+SRC_DIR = pathlib.Path(__file__).parent.parent / 'main'
+sys.path.insert(0, str(SRC_DIR.absolute()))
 
 from anovos.shared.spark import init_spark, SPARK_JARS_PACKAGES
 
 
 @pytest.fixture(scope="session")
 def spark_session() -> SparkSession:
-    _spark, _spark_context, _sql_context = init_spark(app_name="anovos_test")
-    _spark.config("spark.jars.packages", ",".join(list(SPARK_JARS_PACKAGES)))
+    configs = {
+        "app_name": "Anovos_test_pipeline",
+        "jars_packages": SPARK_JARS_PACKAGES,
+        "py_files": [],
+        "spark_config": {
+            "spark.sql.session.timeZone": "GMT",
+            "spark.python.profile": "false",
+        },
+    }
+    _spark, _spark_context, _sql_context = init_spark(**configs)
     return _spark
