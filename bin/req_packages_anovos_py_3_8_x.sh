f1a9533852408402b17b3e00c3d6cc549b510007
Kilian Kluge <dev@kluge.ai>
2022-02-08 19:25:02 +0100

    Clean up bin directory

--- bin/req_packages_anovos_py_3_8_x.sh
+++
@@ -1,4 +0,0 @@
-sudo pip3 install --upgrade pip --user
-sudo pip3 install --upgrade setuptools --user
-sudo pip3 install -r ../requirements.txt
-sudo pip3 install --no-deps --force-reinstall matplotlib==3.4.3
--- bin/setup_on_aws_emr_below_5_30.sh
+++ bin/setup_on_aws_emr_below_5_30.sh
@@ -1,5 +1,4 @@
+sudo pip-3.7 install --upgrade pip setuptools wheel --user
 sudo pip-3.7 install --upgrade cython
-sudo pip-3.7 install --upgrade pip --user
-sudo pip-3.7 install --upgrade setuptools --user
 sudo pip-3.7 install -r ../requirements.txt
 sudo pip-3.7 install --no-deps --force-reinstall matplotlib==3.4.3
