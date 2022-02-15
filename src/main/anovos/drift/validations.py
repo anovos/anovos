e70dca93dbed241b03560e31ee341280d1555a52
ziedbouf <zied.boufaden@gmail.com>
2022-02-15 15:15:51 +0100

    remove deprecated code

--- src/main/anovos/drift/validations.py
+++ src/main/anovos/drift/validations.py
@@ -5,45 +5,6 @@ from loguru import logger
 from anovos.shared.utils import attributeType_segregation
 
 
-class CheckListOfCols(object):
-    def __init__(
-        self,
-        func,
-        arg_cols="list_of_cols",
-        arg_idf_target="idf_target",
-        arg_drop_cols="drop_cols",
-    ):
-        self.func = func
-        self.arg_col = arg_cols
-        self.arg_idf_target = arg_idf_target
-        self.arg_drop_cols = arg_drop_cols
-
-    def __call__(self, *args, **kwargs):
-        cols = args[self.arg_col]
-        target = args[self.arg_idf_target]
-        drops = args[self.arg_drop_cols]
-
-        if cols == "all" and target is not None:
-            num_cols, cat_cols, other_cols = attributeType_segregation(target)
-            cols = num_cols + cat_cols
-
-        if isinstance(cols, str):
-            cols = [x.strip() for x in cols.split("|")]
-
-        if isinstance(drops, str):
-            drops = [x.strip() for x in drops.split("|")]
-
-        cols = list(set([e for e in cols if e not in drops]))
-
-        if (len(cols) == 0) | any(x not in target.columns for x in cols):
-            raise TypeError("Invalid input for Column(s)")
-
-        args[self.arg_col] = cols
-        args[self.arg_drop_cols] = drops
-
-        return self.func
-
-
 def check_list_of_columns(
     columns="list_of_cols",
     target="idf_target",
