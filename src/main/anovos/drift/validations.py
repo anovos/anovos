3cd4365536e881be55e607ea3334654738fc39fc
ziedbouf <zied.boufaden@gmail.com>
2022-02-16 15:31:48 +0100

    fix validation decorator and add logger to main.py

--- src/main/anovos/drift/validations.py
+++ src/main/anovos/drift/validations.py
@@ -1,4 +1,4 @@
-from functools import wraps
+from functools import wraps, partial
 
 from loguru import logger
 
@@ -6,57 +6,61 @@ from anovos.shared.utils import attributeType_segregation
 
 
 def check_list_of_columns(
+    func=None,
     columns="list_of_cols",
     target="idf_target",
     drop="drop_cols",
 ):
-    def _decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            logger.debug("check the list of columns")
+    if func is None:
+        return partial(check_list_of_columns, columns=columns, target=target, drop=drop)
 
-            if args[columns] == "all" and args[target] is not None:
-                num_cols, cat_cols, other_cols = attributeType_segregation(args[target])
-                cols = num_cols + cat_cols
+    @wraps(func)
+    def validate(*args, **kwargs):
+        logger.debug("check the list of columns")
 
-            if isinstance(args[columns], str):
-                cols = [x.strip() for x in args[columns].split("|")]
+        if args[columns] == "all" and args[target] is not None:
+            num_cols, cat_cols, other_cols = attributeType_segregation(args[target])
+            cols = num_cols + cat_cols
 
-            if isinstance(args[drop], str):
-                drops = [x.strip() for x in args[drop].split("|")]
+        if isinstance(args[columns], str):
+            cols = [x.strip() for x in args[columns].split("|")]
 
-            cols = list(set([e for e in cols if e not in drops]))
+        if isinstance(args[drop], str):
+            drops = [x.strip() for x in args[drop].split("|")]
 
-            if (len(cols) == 0) | any(x not in args[target].columns for x in cols):
-                raise TypeError("Invalid input for Column(s)")
+        cols = list(set([e for e in cols if e not in drops]))
 
-            args[columns] = cols
-            args[drop] = drops
+        if (len(cols) == 0) | any(x not in args[target].columns for x in cols):
+            raise TypeError("Invalid input for Column(s)")
 
-        return wrapper
+        args[columns] = cols
+        args[drop] = drops
 
-    return _decorator
+        return func(*args, **kwargs)
+
+    return validate
 
 
-def check_distance_method(argument_name="methods_type"):
-    def _decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            dist_distance_methods = args[argument_name]
+def check_distance_method(func=None, param="method_type"):
 
-            if isinstance(dist_distance_methods, str):
-                if dist_distance_methods == "all":
-                    dist_distance_methods = ["PSI", "JSD", "HD", "KS"]
-                else:
-                    dist_distance_methods = [
-                        x.strip() for x in dist_distance_methods.split("|")
-                    ]
+    if func is None:
+        return partial(check_distance_method, argument_name=param)
 
-            if any(x not in ("PSI", "JSD", "HD", "KS") for x in dist_distance_methods):
-                raise TypeError(f"Invalid input for {argument_name}")
+    @wraps(func)
+    def validate(*args, **kwargs):
+        dist_distance_methods = args[param]
 
-            return func(*args, **kwargs)
+        if isinstance(dist_distance_methods, str):
+            if dist_distance_methods == "all":
+                dist_distance_methods = ["PSI", "JSD", "HD", "KS"]
+            else:
+                dist_distance_methods = [
+                    x.strip() for x in dist_distance_methods.split("|")
+                ]
 
-        return wrapper
+        if any(x not in ("PSI", "JSD", "HD", "KS") for x in dist_distance_methods):
+            raise TypeError(f"Invalid input for {param}")
 
-    return _decorator
+        return func(*args, **kwargs)
+
+    return validate
