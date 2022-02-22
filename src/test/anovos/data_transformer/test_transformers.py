d3185b46c1731ea7bd2b3f42a6e033a2712cf1f4
Sumit Gaurav <sumitgaurav@mobilewalla.com>
2022-02-22 23:28:32 +0530

    Modified test cases according to latest chances in auto imputation functions

--- src/test/anovos/data_transformer/test_transformers.py
+++ src/test/anovos/data_transformer/test_transformers.py
@@ -162,14 +162,14 @@ def test_imputation_MMM(spark_session):
 def test_auto_imputation(spark_session):
     df = read_dataset(spark_session, sample_parquet, "parquet")
     odf = auto_imputation(spark_session, df, list_of_cols=["age","fnlwgt","hours-per-week","relationship","race"],  id_col="ifa")
-    assert len(odf[0].columns) == 18
-    assert odf[0].where(F.col("age").isNull()).count() == 0
-    assert odf[0].where(F.col("fnlwgt").isNull()).count() == 0
-    assert odf[0].where(F.col("hours-per-week").isNull()).count() == 0
-    assert odf[0].where(F.col("race").isNull()).count() == 0
-    assert odf[0].where(F.col("relationship").isNull()).count() == 0
-    assert odf[0].where(F.col("logfnl").isNull()).count() == 10154
-    assert odf[0].where(F.col("education").isNull()).count() == 253
+    assert len(odf.columns) == 18
+    assert odf.where(F.col("age").isNull()).count() == 0
+    assert odf.where(F.col("fnlwgt").isNull()).count() == 0
+    assert odf.where(F.col("hours-per-week").isNull()).count() == 0
+    assert odf.where(F.col("race").isNull()).count() == 0
+    assert odf.where(F.col("relationship").isNull()).count() == 0
+    assert odf.where(F.col("logfnl").isNull()).count() == 10154
+    assert odf.where(F.col("education").isNull()).count() == 253
 
     odf = auto_imputation(spark_session, df, list_of_cols=[],  id_col="ifa")
     assert odf.where(F.col("age").isNull()).count() == 30
@@ -179,7 +179,7 @@ def test_auto_imputation(spark_session):
     assert odf.where(F.col("relationship").isNull()).count() == 4
 
     odf = auto_imputation(spark_session, df, list_of_cols=["age","fnlwgt","hours-per-week","relationship","race"],  id_col="ifa", output_mode="append")
-    assert len(odf[0].columns) == 21
+    assert len(odf.columns) == 21
 
 
 #latent_features
