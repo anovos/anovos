1eba14a8ac6cd2a575e87916213b9b473c39a9b9
Vishnu Gowthem Thangaraj <vishnug@mobilewalla.com>
2022-03-01 21:02:47 +0800

    changes to aws files

--- bin/aws_bootstrap_files/setup_on_aws_emr_below_5_30.sh
+++
@@ -1,5 +0,0 @@
-aws s3 cp s3://<bucket_name>/requirements.txt .
-sudo pip-3.7 install --upgrade pip setuptools wheel --user
-sudo pip-3.7 install --upgrade cython
-sudo pip-3.7 install -r ./requirements.txt
-sudo pip-3.7 install --no-deps --force-reinstall matplotlib==3.4.3
