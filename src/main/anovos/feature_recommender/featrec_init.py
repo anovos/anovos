b07b219ba2f26480d42fb8fbb2832b3ed6474fd4
dattranm <dattranmanh0410@gmail.com>
2022-03-01 16:45:57 -0500

    Change the location of the preloaded csv file: - Now it will be included inside the package, instead of taking it from github resource

--- src/main/anovos/feature_recommender/featrec_init.py
+++ src/main/anovos/feature_recommender/featrec_init.py
@@ -5,6 +5,7 @@ from re import finditer
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from torch.hub import _get_torch_home
+import site
 
 
 def detect_model_path():
@@ -65,7 +66,10 @@ def init_input_fer():
     -------
     Loading the Feature Explorer and Recommender (FER) Input DataFrame (FER corpus)
     """
-    input_path_fer = "https://raw.githubusercontent.com/anovos/anovos/main/data/feature_recommender/flatten_fr_db.csv"
+    site_path = site.getsitepackages()[0]
+    input_path_fer = os.path.join(
+        site_path, "anovos/feature_recommender/data/flatten_fr_db.csv"
+    )
     df_input_fer = pd.read_csv(input_path_fer)
     return df_input_fer
 
