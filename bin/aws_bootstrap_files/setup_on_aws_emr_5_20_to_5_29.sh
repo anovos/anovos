330ee526785bf44764a51a7886b5ea59109ad1b8
Vishnu Gowthem Thangaraj <vishnug@mobilewalla.com>
2022-03-02 09:28:09 +0800

    modified aws_bootstrap folder and readme

--- bin/aws_bootstrap_files/setup_on_aws_emr_5_20_to_5_29.sh
+++
@@ -1,9 +0,0 @@
-aws s3 cp s3://<bucket_name>/requirements.txt .
-sudo pip-3.6 install --upgrade cython
-sudo pip-3.6 install --upgrade pip-3.6 setuptools wheel --user
-sudo pip-3.6 install -r ./requirements.txt
-sudo pip-3.6 install --upgrade --no-deps --force-reinstall scikit-learn
-sudo pip-3.6 install numpy==1.18.1
-sudo pip-3.6 install pandas==1.1.5
-sudo pip-3.6 install tensorflow
-sudo pip-3.6 install --no-deps --force-reinstall matplotlib==3.3.4
