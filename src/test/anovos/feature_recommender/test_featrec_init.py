3300aab77c08830f57330679a4a7748af72f2460
dattranm <dattranmanh0410@gmail.com>
2022-03-07 16:29:35 -0500

    Add unit tests for Feature Recommender: -Unit test for featrec_init functions -Unit test for feature_exploration functions -Unit test for feature_recommendation functions -Small typo fix for feature_recommender_demo example notebook

---
+++ src/test/anovos/feature_recommender/test_featrec_init.py
@@ -0,0 +1,42 @@
+import pytest
+import pandas as pd
+from anovos.feature_recommender.featrec_init import (
+    get_column_name,
+    camel_case_split,
+    recommendation_data_prep,
+)
+
+sample_data_2 = "./data/feature_recommender/test_input_fr_2.csv"
+
+
+@pytest.fixture
+def example_attr_2():
+    return pd.read_csv(sample_data_2)
+
+
+def test_get_column_name(example_attr_2):
+    feature_name, feature_desc, industry, usecase = get_column_name(example_attr_2)
+    assert feature_name == "Name"
+    assert feature_desc == "Desc"
+    assert industry == "Industry"
+    assert usecase == "Usecase"
+
+
+def test_camel_case_split():
+    test_input_1 = "accountWeeks"
+    test_output_1 = camel_case_split(test_input_1)
+    assert test_output_1 == "account Weeks "
+
+    test_input_2 = "account Weeks"
+    test_output_2 = camel_case_split(test_input_2)
+    assert test_output_2 == "account Weeks "
+
+
+def test_recommendation_data_prep(example_attr_2):
+    list_test, test_df = recommendation_data_prep(example_attr_2, "Name", "Desc")
+    for i in range(len(list_test)):
+        assert "_" not in list_test[i]
+
+    for j in range(len(test_df)):
+        assert "_" not in test_df.iloc[:, 0][j]
+        assert test_df.iloc[:, 0][j].strip() == test_df.iloc[:, 0][j]
---
+++ src/test/anovos/feature_recommender/test_feature_exploration.py
@@ -0,0 +1,173 @@
+import pandas as pd
+import pytest
+from anovos.feature_recommender.feature_exploration import (
+    list_all_industry,
+    list_all_usecase,
+    list_all_pair,
+    process_usecase,
+    process_industry,
+    list_usecase_by_industry,
+    list_industry_by_usecase,
+    list_feature_by_industry,
+    list_feature_by_usecase,
+    list_feature_by_pair,
+)
+
+
+def test_list_all_industry():
+    test_df = list_all_industry()
+    assert len(test_df) > 0
+    assert "Industry" in test_df.columns
+    assert test_df.iloc[:, 0].nunique() == len(test_df)
+    for i in range(len(test_df)):
+        assert "," not in test_df.iloc[i, 0]
+
+
+def test_list_all_usecase():
+    test_df = list_all_usecase()
+    assert len(test_df) > 0
+    assert "Usecase" in test_df.columns
+    assert test_df.iloc[:, 0].nunique() == len(test_df)
+    for i in range(len(test_df)):
+        assert "," not in test_df.iloc[i, 0]
+
+
+def test_list_all_pair():
+    test_df = list_all_pair()
+    assert len(test_df) > 0
+    assert "Industry" in test_df.columns
+    assert "Usecase" in test_df.columns
+    assert test_df.groupby(["Industry", "Usecase"]).ngroups == len(test_df)
+
+
+def test_process_usecase():
+    assert process_usecase("fraud", semantic=True) == "fraud detection"
+    assert process_usecase("fraud", semantic=False) == "fraud"
+
+
+def test_process_industry():
+    assert process_industry("telco", semantic=True) == "telecommunication"
+    assert process_industry("telco", semantic=False) == "telco"
+
+
+def test_list_usecase_by_industry():
+    test_df = list_usecase_by_industry("telecommunication")
+    assert len(test_df) > 0
+    assert "Usecase" in test_df.columns
+    assert test_df.iloc[:, 0].nunique() == len(test_df)
+    for i in range(len(test_df)):
+        assert "," not in test_df.iloc[i, 0]
+
+    test_df_2 = list_usecase_by_industry("health")
+    assert len(test_df_2) > 0
+    assert "Usecase" in test_df_2.columns
+    assert test_df_2.iloc[:, 0].nunique() == len(test_df_2)
+    for i in range(len(test_df_2)):
+        assert "," not in test_df_2.iloc[i, 0]
+
+    test_df_3 = list_usecase_by_industry("care", semantic=False)
+    assert len(test_df_3) == 0
+
+
+def test_list_industry_by_usecase():
+    test_df = list_industry_by_usecase("customer churn prediction")
+    assert len(test_df) > 0
+    assert "Industry" in test_df.columns
+    assert test_df.iloc[:, 0].nunique() == len(test_df)
+    for i in range(len(test_df)):
+        assert "," not in test_df.iloc[i, 0]
+
+    test_df_2 = list_industry_by_usecase("fraud")
+    assert len(test_df_2) > 0
+    assert "Industry" in test_df_2.columns
+    assert test_df_2.iloc[:, 0].nunique() == len(test_df_2)
+    for i in range(len(test_df_2)):
+        assert "," not in test_df_2.iloc[i, 0]
+
+    test_df_3 = list_industry_by_usecase("prediction", semantic=False)
+    assert len(test_df_3) == 0
+
+
+def test_list_feature_by_industry():
+    test_df = list_feature_by_industry("telecommunication", semantic=False)
+    assert len(test_df) > 0
+    assert "Usecase" in test_df.columns
+    assert "Industry" in test_df.columns
+    assert "Feature_Name" in test_df.columns
+    assert "Feature_Description" in test_df.columns
+    for i in range(len(test_df)):
+        assert "," not in test_df.iloc[i, 2]
+        assert "," not in test_df.iloc[i, 3]
+        assert "telecommunication" in test_df.iloc[i, 2]
+
+    test_df_2 = list_feature_by_industry("bank", num_of_feat=10)
+    assert len(test_df_2) == 10
+    assert "Usecase" in test_df_2.columns
+    assert "Industry" in test_df_2.columns
+    assert "Feature_Name" in test_df_2.columns
+    assert "Feature_Description" in test_df_2.columns
+    for i in range(len(test_df_2)):
+        assert "," not in test_df_2.iloc[i, 2]
+        assert "," not in test_df_2.iloc[i, 3]
+        assert "banking" in test_df_2.iloc[i, 2]
+
+    test_df_3 = list_feature_by_industry("game", semantic=False)
+    assert len(test_df_3) == 0
+
+
+def test_list_feature_by_usecase():
+    test_df = list_feature_by_usecase("customer churn prediction", semantic=False)
+    assert len(test_df) > 0
+    assert "Usecase" in test_df.columns
+    assert "Industry" in test_df.columns
+    assert "Feature_Name" in test_df.columns
+    assert "Feature_Description" in test_df.columns
+    for i in range(len(test_df)):
+        assert "," not in test_df.iloc[i, 2]
+        assert "," not in test_df.iloc[i, 3]
+        assert "customer churn prediction" in test_df.iloc[i, 3]
+
+    test_df_2 = list_feature_by_usecase("affinity", num_of_feat="all")
+    assert len(test_df_2) > 0
+    assert "Usecase" in test_df_2.columns
+    assert "Industry" in test_df_2.columns
+    assert "Feature_Name" in test_df_2.columns
+    assert "Feature_Description" in test_df_2.columns
+    for i in range(len(test_df_2)):
+        assert "," not in test_df_2.iloc[i, 2]
+        assert "," not in test_df_2.iloc[i, 3]
+        assert "brand" in test_df_2.iloc[i, 3]
+
+    test_df_3 = list_feature_by_usecase("affinity", semantic=False)
+    assert len(test_df_3) == 0
+
+
+def test_list_feature_by_pair():
+    test_df = list_feature_by_pair(
+        "telecommunication", "customer churn prediction", semantic=False
+    )
+    assert len(test_df) > 0
+    assert "Usecase" in test_df.columns
+    assert "Industry" in test_df.columns
+    assert "Feature_Name" in test_df.columns
+    assert "Feature_Description" in test_df.columns
+    for i in range(len(test_df)):
+        assert "," not in test_df.iloc[i, 2]
+        assert "," not in test_df.iloc[i, 3]
+        assert "telecommunication" in test_df.iloc[i, 2]
+        assert "customer churn prediction" in test_df.iloc[i, 3]
+
+    test_df_2 = list_feature_by_pair("bank", "fraud detection", num_of_feat=10)
+    assert len(test_df_2) == 10
+    assert "Usecase" in test_df_2.columns
+    assert "Industry" in test_df_2.columns
+    assert "Feature_Name" in test_df_2.columns
+    assert "Feature_Description" in test_df_2.columns
+    for i in range(len(test_df_2)):
+        assert "," not in test_df_2.iloc[i, 2]
+        assert "," not in test_df_2.iloc[i, 3]
+        assert "banking" in test_df_2.iloc[i, 2]
+        assert "fraud" in test_df_2.iloc[i, 3]
+
+    test_df_3 = list_feature_by_pair("sale", "affinity", semantic=False)
+    assert len(test_df_3) == 0
