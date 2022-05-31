a6d5f31a7e43afa9295ff7d045b078d28275acfb
Mathias Peters <mathias.peters@rtl-extern.de>
2022-05-31 11:08:49 +0200

    added feature service and config checker

--- src/main/anovos/feature_store/feast_exporter.py
+++ src/main/anovos/feature_store/feast_exporter.py
@@ -6,7 +6,6 @@ from black import FileMode, format_str
 from jinja2 import Template
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import lit
-from typing import List, Tuple
 
 ANOVOS_SOURCE = "anovos_source"
 
@@ -17,6 +16,19 @@ dataframe_to_feast_type_mapping = {
     "timestamp": "String"
     # TODO: default type
 }
+
+
+def check_feast_configuration(feast_config: dict, repartition_count: int):
+    if repartition_count != 1:
+        raise ValueError('Please, set repartition parameter to 1 in write_main block in your config yml!')
+    if 'file_path' not in feast_config:
+        raise ValueError('Please, provide a path to the anovos feast repository!')
+    if 'entity' not in feast_config:
+        raise ValueError('Please, provide an entity definition in your config yml!')
+    if 'file_source' not in feast_config:
+        raise ValueError('Please, provide a file source definition in your config yml!')
+    if 'feature_view' not in feast_config:
+        raise ValueError('Please, provide a feature view definition in your config yml!')
 
 
 def generate_entity_definition(config: dict) -> str:
@@ -110,6 +122,22 @@ def generate_prefix():
         return prefix
 
 
+def generate_feature_service(service_name: str, view_name: str):
+    service_template_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "templates", "feature_service.txt"
+    )
+
+    with open(service_template_path, 'r') as f:
+        template_string = f.read()
+        service_template = Template(template_string)
+        data = {
+            'feature_service_name': service_name,
+            'view_name': view_name,
+        }
+
+        return service_template.render(data)
+
+
 def generate_feature_description(types: list, feast_config: dict, file_name: str):
     print("Building feature definitions for feature_store")
     prefix = generate_prefix()
@@ -122,6 +150,10 @@ def generate_feature_description(types: list, feast_config: dict, file_name: str
 
     feature_view_config = feast_config['feature_view']
     feature_view = generate_feature_view(types, feature_view_config, entity_config['name'])
+
+    feature_service = \
+        generate_feature_service(feast_config['service_name'], feature_view_config['name']) \
+        if 'service_name' in feast_config else ""
 
     complete_file_path = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), "templates", "complete_file.txt"
@@ -136,6 +168,7 @@ def generate_feature_description(types: list, feast_config: dict, file_name: str
             "file_source": file_source_definition,
             "entity": entity_definition,
             "feature_view": feature_view,
+            "feature_service": feature_service
         }
 
         file_content = complete_file_template.render(data)
@@ -147,8 +180,8 @@ def generate_feature_description(types: list, feast_config: dict, file_name: str
             of.write(file_content)
 
 
-def add_timestamp_columns(idf: DataFrame, feast_config: dict):
+def add_timestamp_columns(idf: DataFrame, feast_file_source__config: dict):
     print("Adding timestamp columns")
     return idf.withColumn(
-        feast_config["timestamp_col"], lit(datetime.now())
-    ).withColumn(feast_config["create_timestamp_col"], lit(datetime.now()))
+        feast_file_source__config["timestamp_col"], lit(datetime.now())
+    ).withColumn(feast_file_source__config["create_timestamp_col"], lit(datetime.now()))
