9dd59a12b553a5e80261ad1331cf795dd3c0fb53
dattranm <dattranmanh0410@gmail.com>
2022-06-14 15:04:26 -0400

    Add Radius of Gyration function (rog_calculation)

--- src/main/anovos/data_transformer/geospatial.py
+++ src/main/anovos/data_transformer/geospatial.py
@@ -1,6 +1,6 @@
 from pyspark.sql import functions as F
 from pyspark.sql import types as T
-from math import pi, sqrt, atan2
+from math import sin, cos, sqrt, atan2, radians, pi
 
 
 def centroid(idf, lat_col, long_col, id_col=None):
@@ -178,4 +178,84 @@ def weighted_centroid(idf, id_col, lat_col, long_col):
         .withColumn("long_centroid", F.lit(long_centroid))
     )
 
+    return odf
+
+
+def rog_calculation(idf, lat_col, long_col, id_col=None):
+    """
+    Calculate Radius of Gyration (in meter) of a given DataFrame, based on its identifier column (if applicable)
+    Parameters
+    ----------
+    idf
+        Input Dataframe
+    lat_col
+        Column in the input DataFrame that contains latitude data
+    long_col
+        Column in the input DataFrame that contains longitude data
+    id_col
+        Column in the input DataFrame that contains identifier for the DataFrame (Default is None)
+    Returns
+    -------
+    odf : DataFrame
+        Output DataFrame, which contains Radius of Gyration (in meter) and identifier (if applicable)
+    """
+    if id_col not in idf.columns and id_col:
+        raise TypeError("Invalid input for id_col")
+    if lat_col not in idf.columns:
+        raise TypeError("Invalid input for lat_col")
+    if long_col not in idf.columns:
+        raise TypeError("Invalid input for long_col")
+
+    def getHaversineDist(lat1, lon1, lat2, lon2):
+
+        R = 6378126  # approximate radius of earth in m
+
+        lat1 = radians(float(lat1))
+        lon1 = radians(float(lon1))
+        lat2 = radians(float(lat2))
+        lon2 = radians(float(lon2))
+
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        distance = R * c
+        return distance
+
+    udf_harver_dist = F.udf(getHaversineDist, T.FloatType())
+
+    if id_col:
+        idf_centroid = centroid(idf, lat_col, long_col, id_col)
+        idf_join = idf_centroid.join(idf, id_col, "inner")
+        idf_calc = idf_join.withColumn(
+            "distance",
+            udf_harver_dist(
+                F.col(lat_col),
+                F.col(long_col),
+                F.col("lat_centroid"),
+                F.col("long_centroid"),
+            ),
+        )
+
+        odf = idf_calc.groupby(id_col).agg(
+            F.mean("distance").alias("radius_of_gyration")
+        )
+    else:
+        centroid_info = centroid(idf, lat_col, long_col, id_col).rdd.collect()
+        lat_centroid = centroid_info[0][0]
+        long_centroid = centroid_info[0][1]
+        idf_join = idf.withColumn("lat_centroid", F.lit(lat_centroid)).withColumn(
+            "long_centroid", F.lit(long_centroid)
+        )
+        idf_calc = idf_join.withColumn(
+            "distance",
+            udf_harver_dist(
+                F.col(lat_col),
+                F.col(long_col),
+                F.col("lat_centroid"),
+                F.col("long_centroid"),
+            ),
+        )
+
+        odf = idf_calc.groupby().agg(F.mean("distance").alias("radius_of_gyration"))
     return odf
