4fa877b3b008111816a6eeac024da967dcb68b10
Li Jinjin <lijinjin@mobilewalla.com>
2022-08-01 17:19:57 +0800

    reformat code of measures_of_shape

--- src/main/anovos/data_analyzer/stats_generator.py
+++ src/main/anovos/data_analyzer/stats_generator.py
@@ -927,7 +927,13 @@ def measures_of_shape(spark, idf, list_of_cols="all", drop_cols=[], print_impact
     list_result = idf.groupby().agg(*exprs).rdd.flatMap(lambda x: x).collect()
     shapes = []
     for i in range(int(len(list_result) / 2)):
-        shapes.append([list_of_cols[i], list_result[i], list_result[i + int(len(list_result) / 2)]])
+        shapes.append(
+            [
+                list_of_cols[i],
+                list_result[i],
+                list_result[i + int(len(list_result) / 2)],
+            ]
+        )
     odf = (
         spark.createDataFrame(shapes, schema=("attribute", "skewness", "kurtosis"))
         .withColumn("skewness", F.round(F.col("skewness"), 4))
