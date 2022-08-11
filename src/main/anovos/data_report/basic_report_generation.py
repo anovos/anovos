1742fdcbcc87a9968bf7546275d3637e18ac666c
Sourjya Sen <sourjya@mobilewalla.com>
2022-08-11 20:02:50 +0530

    black formatting changes

--- src/main/anovos/data_report/basic_report_generation.py
+++ src/main/anovos/data_report/basic_report_generation.py
@@ -8,7 +8,7 @@ import plotly.express as px
 from anovos.data_analyzer.association_evaluator import (
     IG_calculation,
     IV_calculation,
-    correlation_matrix_numerical,
+    correlation_matrix,
     variable_clustering,
 )
 from anovos.data_analyzer.quality_checker import (
@@ -158,7 +158,7 @@ def anovos_basic_report(
     if skip_corr_matrix:
         AA_funcs = [variable_clustering]
     else:
-        AA_funcs = [correlation_matrix_numerical, variable_clustering]
+        AA_funcs = [correlation_matrix, variable_clustering]
     AT_funcs = [IV_calculation, IG_calculation]
     all_funcs = SG_funcs + QC_rows_funcs + QC_cols_funcs + AA_funcs + AT_funcs
 
@@ -431,7 +431,7 @@ def anovos_basic_report(
 
     AA_content = []
     for i in AA_funcs + AT_funcs:
-        if i.__name__ == "correlation_matrix_numerical":
+        if i.__name__ == "correlation_matrix":
             stats = pd.read_csv(ends_with(local_path) + str(i.__name__) + ".csv").round(
                 3
             )
