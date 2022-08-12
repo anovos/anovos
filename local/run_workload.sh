9090de9b670d69974450e8d3054d45b2108a73c3
Kilian Kluge <dev@kluge.ai>
2022-08-12 13:37:28 +0200

    Final tweaks to allow running from arbitrary directories

--- local/run_workload.sh
+++ local/run_workload.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-python rewrite_configuration.py "${1}"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+python "${SCRIPT_DIR}/rewrite_configuration.py" "${1}"
 
 CONFIG_PATH="${PWD}/config.yaml.tmp"
 DATA_ROOT=$(cat data_directory.tmp)
@@ -8,7 +10,8 @@ OUTPUT_ROOT="${PWD}/output/"
 
 [ -d "${OUTPUT_ROOT}". ] || mkdir "${OUTPUT_ROOT}"
 
-docker run anovos-worker \
+docker run \
        --mount type=bind,source="${CONFIG_PATH}",target=/config.yaml \
        --mount type=bind,source="${DATA_ROOT}",target=/data \
-       --mount type=bind,source="${OUTPUT_ROOT}",target=/output
+       --mount type=bind,source="${OUTPUT_ROOT}",target=/output \
+       anovos-worker
