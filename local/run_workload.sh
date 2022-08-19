d0e82f5e9a6a478ce23186096325521b84f832d7
Kilian Kluge <dev@kluge.ai>
2022-08-19 08:18:36 +0200

    Allow to specify Spark and Anovos version

--- local/run_workload.sh
+++ local/run_workload.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 python "${SCRIPT_DIR}/rewrite_configuration.py" "${1}"
 
@@ -10,8 +10,18 @@ OUTPUT_ROOT="${PWD}/output/"
 
 [ -d "${OUTPUT_ROOT}". ] || mkdir "${OUTPUT_ROOT}"
 
+if [ -z "${2}" ]; then
+  IMAGE_NAME="anovos-worker"
+else
+  if [ -z "${3}" ]; then
+    IMAGE_NAME="anovos-worker-${2}"
+  else
+    IMAGE_NAME="anovos-worker-${2}:${3}"
+  fi
+fi
+
 docker run \
-       --mount type=bind,source="${CONFIG_PATH}",target=/config.yaml \
-       --mount type=bind,source="${DATA_ROOT}",target=/data \
-       --mount type=bind,source="${OUTPUT_ROOT}",target=/output \
-       anovos-worker
+  --mount type=bind,source="${CONFIG_PATH}",target=/config.yaml \
+  --mount type=bind,source="${DATA_ROOT}",target=/data \
+  --mount type=bind,source="${OUTPUT_ROOT}",target=/output \
+  "${IMAGE_NAME}"
