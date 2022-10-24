f7099161796e736b3e34fa2a41bf4b9f3fe20aea
Kilian Kluge <dev@kluge.ai>
2022-10-24 09:25:18 +0200

    debug: Reset config, get fix from main

--- src/main/anovos/data_analyzer/association_evaluator.py
+++ src/main/anovos/data_analyzer/association_evaluator.py
@@ -14,17 +14,18 @@ Association between an attribute and binary target is measured by:
 """
 import itertools
 import math
-
-import pyspark
-import pandas as pd
 import warnings
+
+import pandas as pd
+import pyspark
 from phik.phik import spark_phik_matrix_from_hist2d_dict
 from popmon.analysis.hist_numpy import get_2dgrid
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.stat import Correlation
 from pyspark.sql import Window
 from pyspark.sql import functions as F
 from varclushi import VarClusHi
-from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.stat import Correlation
+
 from anovos.data_analyzer.stats_generator import uniqueCount_computation
 from anovos.data_ingest.data_ingest import read_dataset
 from anovos.data_ingest.data_sampling import data_sample
@@ -242,7 +243,7 @@ def variable_clustering(
 
     list_of_cols = [e for e in list_of_cols if e not in remove_cols]
     idf_sample = idf_sample.select(list_of_cols)
-    num_cols, cat_cols, other_cols = attributeType_segregation(idf_sample)
+    cat_cols = attributeType_segregation(idf_sample)[1]
 
     for i in idf_sample.dtypes:
         if i[1].startswith("decimal"):
@@ -250,6 +251,8 @@ def variable_clustering(
     idf_encoded = cat_to_num_unsupervised(
         spark, idf_sample, list_of_cols=cat_cols, method_type="label_encoding"
     )
+    num_cols = attributeType_segregation(idf_encoded)[0]
+    idf_encoded = idf_encoded.select(num_cols)
     idf_imputed = imputation_MMM(spark, idf_encoded, stats_mode=stats_mode)
     idf_imputed.persist(pyspark.StorageLevel.MEMORY_AND_DISK).count()
     idf_sample.unpersist()
